bf782fa86b4eec6479b476a0ce9a5c1692a45703
qrtf <123179743+qrtf@users.noreply.github.com>
2023-01-21 13:40:26 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+const button = document.getElementById('button')
+const form = document.getElementById('form')
+const answer = document.getElementById('answer')
+
+button.addEventListener('click', (e) => {
+    e.preventDefault()
+    const data = {
+        username: username.value,
+        password: password.value
+    }
+    if(password.value === 'caca'){
+        console.log('bienvenido');
+        form.classList.add('off')
+        answer.classList.remove('off')
+        a
+
+    }else{
+        console.log('denegados')
+        window.alert('Contraseña incorrecta. No hagas trampa!')
+    }
+
+    console.log(data)
+})
